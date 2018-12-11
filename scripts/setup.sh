85bec977b68af1881b0029461226f1de5b8655ab
Alked <37265172+Alked@users.noreply.github.com>
2018-12-11 10:04:13 +0800

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -12,6 +12,9 @@ echo 'Internet connection under testing...'
 ping -c5 canvas.sydney.edu.au &> /dev/null
 if [ "$?" == "0" ]; then
 	echo "Connection seems valid";
+	echo "Starting main program";
+	sleep 3;
+	python3 -u main.py
 else
 	echo "Cannot reach canvas.sydney.edu.au";
 fi
