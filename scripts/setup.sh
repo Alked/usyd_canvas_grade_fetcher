05b40d3b9da70168835dbf92d6c41e4584423a4a
alked <alked.tang@gmail.com>
2018-12-11 14:32:14 +0800

    Added the GAME

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 if command -v python3 >/dev/null 2>&1; then
-	echo 'Python3.x has been installed.'
+	echo 'Python3.x has been installed...'
 else
 	echo 'FATAL: Python3.x is not installed.'
 	echo 'Please install Python3.x first via python.org'
+	sleep 2
 	echo 'Script terminated'
 	exit
 fi
@@ -12,11 +13,13 @@ fi
 echo 'Internet connection under testing...'
 ping -c5 13.237.119.117:443 &> /dev/null
 if [ "$?" == "0" ]; then
-	echo "Connection seems valid";
-	echo "Starting main program...";
-	sleep 3;
-	python3 -u main.py
+	echo "Connection seems valid..."
+	sleep 1
+	echo "Starting main program..."
+	sleep 3
+	python3 -u ./main.py
 else
 	echo "Cannot reach canvas.sydney.edu.au";
+	echo "Please check Internet connection."
 fi
 
