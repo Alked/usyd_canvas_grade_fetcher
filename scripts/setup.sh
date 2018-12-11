28bbf51973c702ccd23fcfcb151be68177a5b9ba
alked <alked.tang@gmail.com>
2018-12-11 14:23:42 +0800

    Added the GAME

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,6 +4,7 @@ if command -v python3 >/dev/null 2>&1; then
 	echo 'Python3.x has been installed.'
 else
 	echo 'FATAL: Python3.x is not installed.'
+	echo 'Please install Python3.x first via python.org'
 	echo 'Script terminated'
 	exit
 fi
@@ -12,7 +13,7 @@ echo 'Internet connection under testing...'
 ping -c5 13.237.119.117:443 &> /dev/null
 if [ "$?" == "0" ]; then
 	echo "Connection seems valid";
-	echo "Starting main program";
+	echo "Starting main program...";
 	sleep 3;
 	python3 -u main.py
 else
